ee13059a984e6e0f7e03c7ae8e20ed5a58d69592
Roman <roma.ozivskij@gmail.com>
2026-01-25 00:56:38 +0200

    build: fix 16KB error and upgrade

--- withAndroidFixes.js
+++ withAndroidFixes.js
@@ -1,44 +1,26 @@
-const { withProjectBuildGradle, withAppBuildGradle } = require('@expo/config-plugins');
+const { withAndroidManifest, withAppBuildGradle } = require('@expo/config-plugins');
 
 module.exports = function withAndroidFixes(config) {
-  // 1. Фикс для NDK (Memory Page Size 16KB)
-  config = withAppBuildGradle(config, (config) => {
-    const buildGradle = config.modResults.contents;
-    
-    // Ищем блок android { ... } и вставляем ndkVersion
-    if (!buildGradle.includes('ndkVersion')) {
-      config.modResults.contents = buildGradle.replace(
-        /android\s?{/,
-        `android {
-    ndkVersion "27.0.12077973"`
-      );
-    }
+  // 1. Принудительно разрешаем извлечение нативных библиотек в манифесте
+  config = withAndroidManifest(config, (config) => {
+    const mainApplication = config.modResults.manifest.application[0];
+    mainApplication.$['android:extractNativeLibs'] = 'true'; // Это ключ к решению для 16 КБ
     return config;
   });
 
-  // 2. Фикс для Google Ads (Понижение версии и фикс Kotlin)
-  config = withProjectBuildGradle(config, (config) => {
-    const buildGradle = config.modResults.contents;
-
-    // Добавляем resolutionStrategy, если его нет
-    if (!buildGradle.includes('force "com.google.android.gms:play-services-ads:23.6.0"')) {
-      const fixCode = `
-    configurations.all {
-        resolutionStrategy {
-            force "com.google.android.gms:play-services-ads:23.6.0"
-            force "org.jetbrains.kotlin:kotlin-stdlib:1.9.25"
-            force "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.25"
+  // 2. Настраиваем Gradle для выравнивания страниц
+  config = withAppBuildGradle(config, (config) => {
+    if (config.modResults.contents.includes('android {')) {
+      const packagingFix = `
+        packagingOptions {
+            jniLibs {
+                useLegacyPackaging = true // Включаем для корректного выравнивания
+            }
         }
-    }`;
-      
-      // Вставляем в конец блока allprojects
-      config.modResults.contents = buildGradle.replace(
-        /allprojects\s?{([\s\S]*?)}/,
-        (match) => {
-            // Вставляем перед закрывающей скобкой блока allprojects
-            const lastBrace = match.lastIndexOf('}');
-            return match.substring(0, lastBrace) + fixCode + "\n}";
-        }
+      `;
+      config.modResults.contents = config.modResults.contents.replace(
+        'android {',
+        `android {${packagingFix}`
       );
     }
     return config;
